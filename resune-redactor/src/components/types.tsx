e9ce79820f2acf58f6007dc6b953dd5d81c0b492
strangka <strangka@student.21-school.ru>
2025-07-18 21:31:27 +0700

    del files

--- resune-redactor/src/components/types.tsx
+++
@@ -1,8 +0,0 @@
-export type SectionType = 'Experience' | 'Projects' | 'Skills' | 'Certificates' | 'About' | 'Education';
-
-export interface Section {
-    id: string;
-    type: SectionType;
-    data: Record<string, string>;
-
-}
